bc049c840fb3aaa944753a703f405210c33a4b21
EvangelouSotiris <swtevag@gmail.com>
2019-11-14 19:37:49 +0200

    add login handlebars

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const http = require('http');
+const express = require('express');
+const path = require("path");
+const exphbs  = require('express-handlebars');	
+const body_parser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || '3000';  //webapp on localhost:3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended:false}));
+
+app.listen(port, function(){
+	console.log('App started on port '+port);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/' , function(req,res,next){
+	res.render('login', {page_name:'login'});
 });
