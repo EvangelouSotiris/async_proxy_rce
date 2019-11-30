b29027b0cbe88dfef57ee146090fc4923896e459
EvangelouSotiris <swtevag@gmail.com>
2019-11-30 15:52:19 +0200

    mysql stuff - login and register almost done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 const exphbs  = require('express-handlebars');	
 const body_parser = require('body-parser');
-
+const mysql = require('mysql');
 const port = process.env.PORT || '3000';  //webapp on localhost:3000
 
 const app = express();
@@ -17,6 +17,21 @@ app.listen(port, function(){
 	console.log('App started on port '+port);
 });
 
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "swt",
+	database: "masters"
+});
+
+con.connect( function(err) {
+	if (err) throw err;
+	console.log("Connected to mysql login db");
+});
+
+// GET AND POST METHODS
+
 app.get('/' , function(req,res){
 	res.render('login', {page_name:'login'});
 });
@@ -25,8 +40,34 @@ app.post('/masterlog' , function(req, res){
 	let master = req.body.master_name;
 	let pass = req.body.master_pass;
 	//Login checking logic
-	
-	//
+	con.connect(function(err) {
+		con.query("select count(username) from master_info where username = '" + master + "' and password = '" + pass +"'", function (err, result, fields) {
+    			if (err) { throw err;}
+			console.log(result);
+			if (result[0]['count(username)'] == 0){
+				res.render('login', {page_name:'login', error:'The master you entered doesn\'t exist. Click accept to create this master account.' , master:master, pass:pass});
+			}
+			else {
+				//create the acc and log in after asking if its okay
+				res.render('master_console', {page_name: master + '\'s console', master_name: master});
+			}
+		});
+	});	
+});
+
+//TODO
+//1) ALLAGH TOU QUERY ME COUNT GIATI TWRA KAI O XRISTIS NA YPARXEI PALI THA VGALEI ERROR OTI DEN YPARXEI
+//2) NA VALOUME TO TS_LOGIN PEDIO NA EXEI THN WRA KANONIKA ME KWDIKA
+//3) OTAN KANEI LOGIN IN TO LOGGED NA GINETAI 1 KAI NA VALOUME LOGOUT POU KANEI TO LOGGED KSANA 0 (H AN EKLEISE TO SESSION)
+
+app.post('/create_and_log', function(req,res){
+	let master = req.body.master_name;
+	let pass = req.body.master_pass;
+	con.connect(function(err) {
+		con.query("insert into master_info (username, password, ts_login, logged) VALUES ('" + master + "', '" + pass + "', '15:14:14', '1')", function (err, result, fields) {
+    			if (err) { throw err;}
+		});
+	});
 	res.render('master_console', {page_name: master + '\'s console', master_name: master});
 });
 
