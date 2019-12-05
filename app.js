00c3a2f15d100a68c5ce513209a03ec249badfc7
johneliades <johneliades@hotmail.com>
2019-12-05 22:47:57 +0200

    Login: removed outline from input

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ app.set('view engine', 'handlebars');
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:false}));
 
-app.listen(port, function(){
+app.listen(port, function() {
 	console.log('App started on port '+port);
 });
-
 
 var con = mysql.createConnection({
 	host: "localhost",
@@ -32,7 +31,7 @@ con.connect( function(err) {
 
 // GET AND POST METHODS
 
-app.get('/' , function(req,res){
+app.get('/' , function(req,res) {
 	res.render('login', {page_name:'login'});
 });
 
@@ -44,17 +43,19 @@ function return_formatted_date() {
 	let hours = date_ob.getHours();
 	let minutes = date_ob.getMinutes();
 	let seconds = date_ob.getSeconds();
-	return(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
-} 
 
-app.post('/masterlog' , function(req, res){
+	return(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+}
+
+app.post('/masterlog' , function(req, res) {
 	let master = req.body.master;
 	let pass = req.body.master_pass;
 	//Login checking logic
 	con.connect(function(err) {
 		con.query("select count(username) from master_info where username = '" + master + "' and password = '" + pass +"'", function (err, result, fields) {
-    			if (err) { throw err;}
-			if (result[0]['count(username)'] == 0){
+			if (err) { throw err;}
+
+			if (result[0]['count(username)'] == 0) {
 				//create the acc and log in after asking if its okay
 				res.render('login', {page_name:'login', error:'The master you entered doesn\'t exist. Click accept to create this master account.' , master:master, pass:pass});
 			}
@@ -70,23 +71,23 @@ app.post('/masterlog' , function(req, res){
 //TODO
 //1) ALLAGH TOU QUERY ME COUNT GIATI TWRA KAI O XRISTIS NA YPARXEI PALI THA VGALEI ERROR OTI DEN YPARXEI
 
-app.post('/create_and_log', function(req,res){
+app.post('/create_and_log', function(req,res) {
 	let master = req.body.master;
 	let pass = req.body.master_pass;
 	con.connect(function(err) {
 		con.query("insert into master_info (username, password, ts_login, logged) VALUES ('" + master + "', '" + pass + "', '" + return_formatted_date() + "', '1')", function (err, result, fields) {
-    			if (err) { throw err;}
+    			if (err) { throw err; }
 		});
 	});
 	res.render('master_console', {page_name: master + '\'s console', master: master});
 });
 
-app.post('/command_handler' , function(req, res){
+app.post('/command_handler' , function(req, res) {
 	let new_command = req.body.new_command;
 });
 
 
-app.post('/logout', function(req,res){
+app.post('/logout', function(req,res) {
 	let master = req.body.master;
 	console.log(master);
 	con.connect(function(err) {
